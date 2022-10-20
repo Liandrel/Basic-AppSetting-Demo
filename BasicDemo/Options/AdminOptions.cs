81e09eefa75d6c30c4da1e8c776dc39b410f4e30
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-20 20:03:47 +0200

    Add project files.

---
+++ BasicDemo/Options/AdminOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicDemo.Options
+{
+    public class AdminOptions
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
---
+++ BasicDemo/Options/EmailSettingsOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicDemo.Options
+{
+    public class EmailSettingsOptions
+    {
+        public bool EnableEmailSystem { get; set; }
+        public int EmailTimeoutSeconds { get; set; }
+        public List<string> EmailServers { get; set; }
+        public AdminOptions EmailAdmin { get; set; }
+    }
+}
